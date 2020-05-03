1c6f49af3a25b354a5391595fc05b9a2120e49b2
Trevor Low <tlow94@uw.edu>
2020-05-03 00:19:39 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,7 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    self.characters.map do {|x| "#{x.name} - #{x.show.name}"} #?
+    self.characters.map do |x| "#{x.name} - #{x.show.name}" #?
     end
   end
 end
