cf5ec1a7d199bdae0f9fa54044b8cb5bb05e3d99
Trevor Low <tlow94@uw.edu>
2020-05-03 00:09:37 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
@@ -7,7 +6,6 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    binding.pry
     self.show
   end
 end
