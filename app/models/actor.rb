d102c8abd2328ec9930f919b7d9c7da5c1ce1a61
Trevor Low <tlow94@uw.edu>
2020-05-03 00:32:38 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,12 +1,14 @@
 class Actor < ActiveRecord::Base
-  has_many :characters
-  has_many :shows, through: :characters
+    has_many :characters
+    has_many :shows, through: :characters
 
-  def full_name
-    "#{self.first_name} #{self.last_name}"
-  end
-  def list_roles
-    self.characters.map do |x| "#{x.name} - #{x.show.name}"
+    def full_name
+        "#{self.first_name} #{self.last_name}"
     end
-  end
+
+    def list_roles
+        self.characters.map do |char|
+           "#{char.name} - #{char.show.name}"
+        end
+    end
 end
